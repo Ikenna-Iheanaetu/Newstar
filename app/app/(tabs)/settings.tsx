1ae3a6c1f4287093667ae90fba0d09fd51d6f5a0
Ikenna Iheanaetu <ikennadeveloper@gmail.com>
2024-10-10 20:48:24 +0100

    I am done with the settings screen all that remains is to make the toggle actually work

--- app/app/(tabs)/settings.tsx
+++ app/app/(tabs)/settings.tsx
@@ -1,10 +1,46 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Pressable, Switch } from "react-native";
+import React, { useState } from "react";
+import { Colors } from "@/constants/Colors";
 
 export default function Settings() {
+  const [enabled, setEnabled] = useState<boolean>(false);
+  const toggleSwitch = () => {
+    setEnabled(!enabled);
+  };
+
   return (
-    <View>
-      <Text>Settings Screen</Text>
+    <View style={styles.container}>
+      <Pressable style={styles.itemBtn}>
+        <Text style={styles.itemText}>Dark Theme</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#3e3e3e" }}
+          thumbColor={enabled ? "#f5dd4b" : "#f3f3f4"}
+          ios_backgroundColor={"#3e3e3e"}
+          onValueChange={toggleSwitch}
+          value={enabled}
+          style={{ transform: [{ scale: 1 }] }}
+        />
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  itemBtn: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: Colors.white,
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+  },
+  itemText: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: Colors.black,
+  },
+});
